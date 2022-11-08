49e5ee5325096c3f35401e568f80d401a34de12b
Georgio Sorin <georgio.sorin@gmail.com>
2022-11-08 11:43:46 +0100

    first commit

---
+++ server-raw.js
@@ -0,0 +1,28 @@
+import http from 'http'
+
+const server = http.createServer((request,response) => {
+    if (request.url === '/api/products' && request.method === 'GET') {
+        response.writeHead(200, {'Content-type': 'application/json'})
+        response.end(JSON.stringify(
+            {
+                id:1,
+                name: "Iphone",
+                color: "Black"
+            }
+        ))
+    } else {
+        response.writeHead(404, {'Content-type': 'application/json'})
+        response.end(JSON.stringify(
+            {
+                message: "route not found, please /api/products",
+            }
+        ))
+    }
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+    console.log(`Server is still running on port : ${ PORT }`)
+    console.log(`Lien : http://localhost:${ PORT }`)
+})
